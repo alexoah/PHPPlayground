6e10fc8b6583a3a68b70772e1c3eb4ac9d1e8ba0
Alexoah YT <alexandra.soah@gmail.com>
2020-03-30 16:58:14 +0700

    added answer files for PHP Variables exercises

---
+++ W3School-PHPExercises/PHP-Variables/phpVariablesE1.php
@@ -0,0 +1,10 @@
+/**
+ *	from PHP Variables: Exercise 1 ( https://www.w3schools.com/php/exercise.asp?filename=exercise_variables1 )
+ *
+ *	question:
+ *	Create a variable named txt and assign the value "Hello".
+ *
+ *	____ = "_____";
+ *
+ */
+$txt = "Hello";
---
+++ W3School-PHPExercises/PHP-Variables/phpVariablesE2.php
@@ -0,0 +1,14 @@
+/**
+ *	from PHP Variables: Exercise 2 ( https://www.w3schools.com/php/exercise.asp?filename=exercise_variables2 )
+ *
+ *	question:
+ *	Create one variable named x, and one variable named y, then use the echo statement to output the sum of x and y.
+ *	
+ *	__ = 5;
+ *	__ = 7;
+ *	____ __ + __;
+ *
+ */
+$x = 5;
+$y = 7;
+echo $x + $y;
